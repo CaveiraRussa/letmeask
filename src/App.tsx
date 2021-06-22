6dcc7fd7c90ee3a4e4049679f08f78c23ccc18fa
Luiz Santiago <luiz.santiago@championx.com>
2021-06-22 01:26:57 -0300

    WIP: Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Button } from './components/Button';
+import { Home } from './pages/Home';
 
 function App() {
-  return (
-    <div>
-    <Button/>
-    <Button/>
-    <Button/>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
-
