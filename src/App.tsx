b3ff1a19917f778962ced2c904c2ecbb20645d5f
Luiz Santiago <luiz.santiago@championx.com>
2021-06-23 01:22:59 -0300

    WIP: Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import { createContext } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { NewRoom } from './pages/NewRoom';
 import { Home } from './pages/Home';
 
+export const TestContext = createContext('');
+
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <TestContext.Provider value={'teste'}>
+        <Route path='/' exact component={Home}/>
+        <Route path='/rooms/new' component={NewRoom}/>
+      </TestContext.Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,32 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import { Button } from '../components/Button';
+import { auth, firebase } from '../services/firebase';
+
+import { TestContext } from '../App';
+
 import '../styles/auth.scss';
 
 export function Home() {
+  const history = useHistory();
+  const value = useContext(TestContext);
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      history.push('/rooms/new');
+    });
+  }
+
   return (
-    <div id="page-auth">
+    <div id='page-auth'>
       <aside>
         <img
           src={illustrationImg}
@@ -16,16 +36,16 @@ export function Home() {
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div className="main-content">
+        <div className='main-content'>
           <img src={logoImg} alt='Letmeask' />
-          <button>
+          <button onClick={handleCreateRoom} className='create-room'>
             <img src={googleIconImg} alt='Logo do Google' />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className='separator'>ou entre em uma sala</div>
           <form>
             <input type='text' placeholder='Digite o código da sala' />
-            <button type='submit'>Entrar na sala</button>
+            <Button type='submit'>Entrar na sala</Button>
           </form>
         </div>
       </main>
