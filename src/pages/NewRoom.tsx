d468518d91f29eb1ac39044f29666f46f14bea68
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-07-01 00:25:52 -0300

    WIP: Aula 3

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -6,14 +6,33 @@ import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
 
 import { Button } from '../components/Button';
+import { FormEvent, useState } from 'react';
 
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 export function NewRoom() {
   const {user} = useAuth();
 
+  const [newRoom, setNewRoom] = useState('');
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim()===''){
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+  }
+
   return (
-    <div id="page-auth">
+    <div id='page-auth'>
       <aside>
         <img
           src={illustrationImg}
@@ -23,14 +42,19 @@ export function NewRoom() {
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div className="main-content">
+        <div className='main-content'>
           <img src={logoImg} alt='Letmeask' />
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type='text' placeholder='Digite o código da sala' />
+          <form onSubmit={handleCreateRoom}>
+            <input type='text' placeholder='Nome da sala' 
+            onChange={event => setNewRoom(event.target.value)}
+            value={newRoom}
+            />
             <Button type='submit'>Criar sala</Button>
           </form>
-          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
+          <p>
+            Quer entrar em uma sala existente? <Link to='/'>clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
